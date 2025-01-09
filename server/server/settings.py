5482cd02128b22c872aef1ecc93e2705aa752283
Olia <oliaopanasiukk@gmail.com>
2025-01-09 19:35:05 +0200

    enable CORS

--- server/server/settings.py
+++ server/server/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MIDDLEWARE.insert(0, 'corsheaders.middleware.CorsMiddleware')
+
+CORS_ALLOW_ALL_ORIGINS = "http://localhost:5173"
