ece13021fc6eff8a60ebc9acc4aa40ba328880d2
Olia <oliaopanasiukk@gmail.com>
2025-01-14 22:33:40 +0200

    enhance about us page styling

--- client/src/pages/about/AboutUsPage.tsx
+++ client/src/pages/about/AboutUsPage.tsx
@@ -11,17 +11,17 @@ const AboutUsPage = () => {
                         a comfortable living environment. We work closely with organizations, shelters, and volunteers.
                     </p>
                 </div>
-                <div className={styles['achievements']}><span className={styles['number']}>1100</span> Pets adopted</div>
+                <div className={styles['achievements']}><span className={styles['number']}>2 500</span> Pets adopted</div>
                 <div className={styles['about-image']}>
-                    <img src="/images/cats/foxy-cats.png" alt="Who We Are" />
+                    <img src="/images/cats/cat-home.png" alt="Who We Are" />
                 </div>
             </div>
 
-            <div className={`${styles['our-mission']} my-10`}>
+            <div className={`${styles['our-mission']} my-7`}>
                 <div className={styles['about-image']}>
-                    <img src="/images/dogs/dog.png" alt="Our Mission" />
+                    <img src="/images/dogs/dog-home.png" alt="Our Mission" />
                 </div>
-                <div className={styles['achievements']}><span className={styles['number']}>500+</span>Volunteers</div>
+                <div className={styles['achievements']}><span className={styles['number']}>200 +</span>Volunteers</div>
                 <div className={styles['about-content']}>
                     <h2>Our Mission</h2>
                     <p>
@@ -31,7 +31,7 @@ const AboutUsPage = () => {
                 </div>
             </div>
 
-            <div className={styles['what-we-do']}>
+            <div className={`${styles['what-we-do']} mt-8`}>
                 <div className={styles['about-content']}>
                     <h2>What We Do</h2>
                     <ul>
@@ -41,9 +41,9 @@ const AboutUsPage = () => {
                         <li>Collaborating with shelters and volunteers.</li>
                     </ul>
                 </div>
-                <div className={styles['achievements']}><span className={styles['number']}>1k</span> Donations raised</div>
+                <div className={`${styles['achievements']} mt-5`}><span className={styles['number']}>1k</span> Donations raised</div>
                 <div className={styles['about-image']}>
-                    <img src="/images/cats/cat.png" alt="What We Do" />
+                    <img src="/images/cats/cat-dog.png" alt="What We Do" />
                 </div>
             </div>
         </div>
